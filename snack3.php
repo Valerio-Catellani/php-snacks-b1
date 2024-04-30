5c5d21af4e7e32c0a7266e7ab99d70ca42c816bf
Valerio-Catellani <valeriocatellani@gmail.com>
2024-04-30 15:44:14 +0200

    created the snack3

---
+++ snack3.php
@@ -0,0 +1,92 @@
+<?php
+/*
+Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. Stampare ogni data con i relativi post.
+*/
+$posts = [
+
+  '10/01/2019' => [
+    [
+      'title' => 'Post 1',
+      'author' => 'Michele Papagni',
+      'text' => 'Testo post 1'
+    ],
+    [
+      'title' => 'Post 2',
+      'author' => 'Michele Papagni',
+      'text' => 'Testo post 2'
+    ],
+  ],
+  '10/02/2019' => [
+    [
+      'title' => 'Post 3',
+      'author' => 'Michele Papagni',
+      'text' => 'Testo post 3'
+    ]
+  ],
+  '15/05/2019' => [
+    [
+      'title' => 'Post 4',
+      'author' => 'Michele Papagni',
+      'text' => 'Testo post 4'
+    ],
+    [
+      'title' => 'Post 5',
+      'author' => 'Michele Papagni',
+      'text' => 'Testo post 5'
+    ],
+    [
+      'title' => 'Post 6',
+      'author' => 'Michele Papagni',
+      'text' => 'Testo post 6'
+    ]
+  ],
+];
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" /> <!-- Link a Font Awesome-->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <!--Link bootstrap-->
+  <link rel="stylesheet" href="resources/css/style.css">
+  <link rel="stylesheet" href="resources/css/animation.css">
+  <link rel="stylesheet" href="resources/css/media-query.css">
+  <script src="resources/js/script.js" type="text/javascript" defer></script>
+  <title>Snack3</title>
+</head>
+
+<body class="bg-dark p-5 text-white">
+  <div class='text-center container border rounded-4 border-warning border-3 text-dark bg-dark-subtle '>
+    <h1 class="mb-4">Snack3</h1>
+    <?php
+    foreach ($posts as $data => $postArrayAssociati) {
+      echo '<div class="mb-5 border border-black bg-warning-subtle rounded-4">' . "<h2>$data</h2>";
+      foreach ($postArrayAssociati as $post) {
+        echo '<div>' .
+          '<h3>' . $post['title'] . '</h3>' .
+          '<p><strong>Autore:</strong> ' . $post['author'] . '</p>' .
+          '<p>' . $post['text'] . '</p>' .
+          '</div>';
+      }
+      echo '</div>';
+    }
+    ?>
+  </div>
+
+
+  <script src="resources/js/hypeUtility.js" type="text/javascript"></script>
+</body>
+
+<style>
+</style>
+
+<script>
+</script>
+
+</html>
